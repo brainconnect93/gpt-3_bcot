b125cc958173d758300627842d993cdd1844f740
brainconnect93 <brainconnect0@gmail.com>
2023-03-14 22:55:10 +0100

    Complete setup the File components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+import { Footer, Blog, Possibility, Feature, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	<div className='App'>
+	  <div className='gradient_bg'>
+		<Navbar />
+		<Header />
+	  </div>
+	  <Brand />
+	  <WhatGPT3 />
+	  <Feature />
+	  <Possibility />
+	  <CTA />
+	  <Blog />
+	  <Footer />
+	</div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Article } from "./article/Article";
+export { default as Brand } from "./brand/Brand";
+export { default as CTA } from "./cta-call/CTA";
+export { default as Features } from "./features/Features";
+export { default as Navbar } from "./navbar/Navbar";
