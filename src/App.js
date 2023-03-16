d3ce57ee64b17691a05ede387aa30240d1bc21af
brainconnect93 <brainconnect0@gmail.com>
2023-03-16 12:31:46 +0100

    Working with the navbar scetion

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 
 import {
   Footer, Blog, Possibility, Feature, WhatGPT3, Header,
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+// import { RiMenu3Line, RiCloseLine } from 'react-icons'
 import './navbar.css';
 
 const Navbar = () => (
-  <div>Navbar</div>
+  <div className="gpt3_navbar">
+    <div className="gpt3_navbar-links">
+      <div className="gpt3_navbar-links_logo">
+        <h2>
+          GPT3_
+          <span>BCOT</span>
+        </h2>
+      </div>
+      <div className="gpt3_navbar-links_container">
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#wgpt3">What is GPT?</a></li>
+          <li><a href="#possibility">Open AI</a></li>
+          <li><a href="#feature">Case Studies</a></li>
+          <li><a href="#blog">Library</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
 );
 
 export default Navbar;
