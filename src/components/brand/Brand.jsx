f2f53218775e68f6e60df22b7ad28e6b4698f4f6
brainconnect93 <brainconnect0@gmail.com>
2023-03-18 16:14:51 +0100

    Add image for page branding

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import {
+  google, slack, bitmap, dropbox, shopify,
+} from './import';
 import './brand.css';
 
 const Brand = () => (
-  <div>
-    
+  <div className="gpt3_brand section_padding">
+    <div>
+      <img src={google} alt="google icon" />
+    </div>
+    <div>
+      <img src={slack} alt="slack icon" />
+    </div>
+    <div>
+      <img src={bitmap} alt="Atlassan icon" />
+    </div>
+    <div>
+      <img src={dropbox} alt="dropbox icon" />
+    </div>
+    <div>
+      <img src={shopify} alt="shopify icon" />
+    </div>
   </div>
 );
 
---
+++ src/components/brand/import.js
@@ -0,0 +1,13 @@
+import google from '../../assets/google.svg';
+import slack from '../../assets/slack.svg';
+import bitmap from '../../assets/Bitmap.svg';
+import dropbox from '../../assets/dropbox.svg';
+import shopify from '../../assets/shopify.svg';
+
+export {
+  google,
+  slack,
+  bitmap,
+  dropbox,
+  shopify,
+};
