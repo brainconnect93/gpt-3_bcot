537c3595e629ca83c58f8563bd6ff8fe9bb61661
brainconnect93 <brainconnect0@gmail.com>
2023-03-18 07:14:28 +0100

    Working with the header section

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,17 +1,26 @@
-// eslint-disable-next-line max-len
-
 import React from 'react';
+import people from '../../assets/people.svg';
+import ai from '../../assets/Header-Illustration.svg';
 import './header.css';
 
 const Header = () => (
   <div className="gpt3_header section_padding" id="Home">
-    <div className="gpt_header-content">
+    <div className="gpt3_header-content">
       <h1 className="gradient_text">Let’s Build Something amazing with GPT-3 OpenAI</h1>
       <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise.</p>
 
       <div>
         <input type="email" placeholder="Enter your email" />
         <button type="button">Get Started</button>
+      </div>
+
+      <div>
+        <img src={people} alt="people visited" />
+        <p>1,600 people requested access a visit in last 24 hours</p>
+      </div>
+
+      <div className="gpt3_header-image">
+        <img src={ai} alt="ai" />
       </div>
     </div>
   </div>
