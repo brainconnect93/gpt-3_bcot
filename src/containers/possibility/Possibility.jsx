97a7d88d81f7a46af765c0cdaa9d12980684a266
brainconnect93 <brainconnect0@gmail.com>
2023-03-20 22:46:03 +0100

    Working with possible responsiveness

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import possibleImg from '../../assets/Feature-image.svg'
+import possibleImg from '../../assets/Feature-Image.svg';
 import './possibility.css';
 
 const Possibility = () => (
   <div className="gpt3_possible section_padding" id="possibility">
-    <div className="gtp3_possible-image">
-      <img src={possibleImg} alt="possibility image" />
+    <div className="gpt3_possible-image">
+      <img src={possibleImg} alt="possibility" />
+    </div>
+    <div className="gpt3_possible-content">
+      <h4>Request Early Access to Get Started</h4>
+      <h1 className="gradient_text">The possibilities are beyond your imagination</h1>
+      <p>
+        Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise.
+      </p>
+      <h4>Request Early Access to Get Started</h4>
     </div>
   </div>
 );
