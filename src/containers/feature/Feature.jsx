a016aad8f6f2e817fa48553d88db8702847266df
brainconnect93 <brainconnect0@gmail.com>
2023-03-20 16:52:33 +0100

    Modify feature for array props

--- src/containers/feature/Feature.jsx
+++ src/containers/feature/Feature.jsx
@@ -1,35 +1,42 @@
 import React from 'react';
+import { Features } from '../../components';
 import './feature.css';
 
-const featureData = [
+const featuresData = [
   {
-    tite: "Improving end distrusts instantly",
-    text: "From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded."
+    title: 'Improving end distrusts instantly',
+    text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.',
   },
   {
-    tite: "Become the tended active",
-    text: "Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to."
+    title: 'Become the tended active',
+    text: 'Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to.',
   },
   {
-    tite: "Message or am nothing",
-    text: "Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address."
+    title: 'Message or am nothing',
+    text: 'Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address.',
   },
   {
-    tite: "Really boy law county",
-    text: "Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush."
-  }
-]
+    title: 'Really boy law county',
+    text: 'Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush.',
+  },
+];
 
 const Feature = () => (
   <div className="gpt3_features section_padding" id="features">
     <div className="gpt3_features-header">
       <h1 className="gradient_text">
-      The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.
+        The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.
       </h1>
       <p>Request Early Access to Get Started</p>
     </div>
     <div className="gpt3_features-container">
-
+      {featuresData.map((item) => (
+        <Features
+          title={item.title}
+          text={item.text}
+          key={item.id}
+        />
+      ))}
     </div>
   </div>
 );
