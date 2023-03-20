90d20e6566c545754f608b182418d61ecc98d4d2
brainconnect93 <brainconnect0@gmail.com>
2023-03-20 23:58:20 +0100

    Start with the blog page

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -2,7 +2,23 @@ import React from 'react';
 import './blog.css';
 
 const Blog = () => (
-  <div>Blog</div>
+  <div className="gpt3_blog section_padding" id="blog">
+    <div className="gpt3_blog-header">
+      <h1 className="gradient_text">A lot is happening, 
+We are blogging about it.</h1>
+    </div>
+    <div className="gpt3_blog-container">
+      <div className="gpt3_blog-container_groupA">
+        Article
+      </div>
+      <div className="gpt3_blog-container_groupA">
+        Article
+        Article
+        Article
+        Article
+      </div>
+    </div>
+  </div>
 );
 
 export default Blog;
