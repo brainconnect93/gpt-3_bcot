8887f3b317848b19db328946587c2fc25de6c43f
brainconnect93 <brainconnect0@gmail.com>
2023-03-20 11:11:04 +0100

    Working around the other features

--- src/containers/feature/Feature.jsx
+++ src/containers/feature/Feature.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
 import './feature.css';
 
+const featureData = [
+  {
+    tite: "Improving end distrusts instantly",
+    text: "From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded."
+  },
+  {
+    tite: "Become the tended active",
+    text: "Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to."
+  },
+  {
+    tite: "Message or am nothing",
+    text: "Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address."
+  },
+  {
+    tite: "Really boy law county",
+    text: "Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush."
+  }
+]
+
 const Feature = () => (
-  <div>Feature</div>
+  <div className="gpt3_features section_padding" id="features">
+    <div className="gpt3_features-header">
+      <h1 className="gradient_text">
+      The Future is Now and You Just Need To Realize It. Step into Future Today & Make it Happen.
+      </h1>
+      <p>Request Early Access to Get Started</p>
+    </div>
+    <div className="gpt3_features-container">
+
+    </div>
+  </div>
 );
 
 export default Feature;
