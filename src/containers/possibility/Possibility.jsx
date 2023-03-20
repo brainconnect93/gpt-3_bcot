e28febc2a3779d5f5843c36149394c2bde199a34
brainconnect93 <brainconnect0@gmail.com>
2023-03-20 19:00:37 +0100

    Start the Poss section with the image

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import possibleImg from '../../assets/Feature-image.svg'
 import './possibility.css';
 
 const Possibility = () => (
-  <div>Possibility</div>
+  <div className="gpt3_possible section_padding" id="possibility">
+    <div className="gtp3_possible-image">
+      <img src={possibleImg} alt="possibility image" />
+    </div>
+  </div>
 );
 
 export default Possibility;
