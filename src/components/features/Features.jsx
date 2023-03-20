a203e92d8af909cd8a5958dda89e28d3364c3d01
brainconnect93 <brainconnect0@gmail.com>
2023-03-20 09:58:39 +0100

    Adding the prop-tpes to the feature

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './features.css';
 
-const Features = () => (
-  <div>Feature</div>
+const Features = ({ title, text }) => (
+  <div className="gpt3_features-container_feature">
+    <div className="gpt3_features-container_feature-title">
+      <div />
+      <h1>{title}</h1>
+    </div>
+    <div className="gpt3_features-container_feature-text">
+      {text}
+    </div>
+  </div>
 );
+
+Features.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+};
 
 export default Features;
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -5,7 +5,10 @@ import './whatGPT3.css';
 const WhatGPT3 = () => (
   <div className="gpt3_wahtgpt3 section_margin" id="wgpt3">
     <div className="gpt3_whatgpt3-feature">
-      <Feature />
+      <Feature
+        title="What is GPT-3"
+        text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+      />
     </div>
 
     <div className="gpt3_whatgpt3-header section_margin">
@@ -14,9 +17,18 @@ const WhatGPT3 = () => (
     </div>
 
     <div className="gpt3_whatgpt3-container">
-      <Feature />
-      <Feature />
-      <Feature />
+      <Feature
+        title="Chatbots"
+        text="We so opinion friends me message as delight. Whole front do of plate heard oh ought."
+      />
+      <Feature
+        title="Knowledgebase"
+        text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments."
+      />
+      <Feature
+        title="Education"
+        text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments."
+      />
     </div>
   </div>
 );
